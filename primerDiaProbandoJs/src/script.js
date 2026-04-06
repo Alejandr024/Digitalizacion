24821a0c8c4b5e4a6024e9980bd6d141b583205d
Ale.G0mez15 <alej3839@gmail.com>
2026-04-06 11:05:51 +0100

    Correción
    Corregido orden de tabulacion es Js.

--- primerDiaProbandoJs/src/script.js
+++ primerDiaProbandoJs/src/script.js
@@ -1,23 +1,23 @@
 function eventoClick(caja) {
- caja.innerText = "Click hecho";
+  caja.innerText = "Click hecho";
 }
 function eventoOver(caja) {
- caja.style.color = "Green";
+  caja.style.color = "Green";
 }
 function eventoOut(caja) {
- caja.style.background = "Red";
+  caja.style.background = "Red";
 }
 function eventoDown(caja) {
- caja.innerText = "Hola mundo";
+  caja.innerText = "Hola mundo";
 }
 function eventoUp(caja) {
- caja.innerText = "Adios mundo";
+  caja.innerText = "Adios mundo";
 }
 function eventoInput(input) {
- console.log(input.value); // Mostar input en consola en vez de mostralo en el front o en la pagina
-// input.nextElementSibling.textContent = "Texto: " + input.value; Mostrar el input escrito en tiempo real, se escribe "texto" parsa que no desaparezca del front al escribir el input. 
+  console.log(input.value); // Mostar input en consola en vez de mostralo en el front o en la pagina
+  // input.nextElementSibling.textContent = "Texto: " + input.value; Mostrar el input escrito en tiempo real, se escribe "texto" parsa que no desaparezca del front al escribir el input.
 }
 
-function eventoDouble(caja){
+function eventoDouble(caja) {
   caja.innerText = "Haz hecho doble click, muy bien.";
 }
