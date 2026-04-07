85e89c2d3202f7d01144796ea8f652d6ffed0393
Ale.G0mez15 <alej3839@gmail.com>
2026-04-07 22:41:45 +0100

    Correción
    Ordenado el HTML, CSS y JavaScript.

--- primerDiaProbandoJs/src/script.js
+++ primerDiaProbandoJs/src/script.js
@@ -1,17 +1,17 @@
 function eventoClick(caja) {
-  caja.innerText = "Click hecho";
+  caja.innerText = "Click hecho"; // Cambia el texto de la caja al hacer click
 }
 function eventoOver(caja) {
-  caja.style.color = "Green";
+  caja.style.color = "Green"; // Cambia el color del texto a verde al pasar el mouse por encima
 }
 function eventoOut(caja) {
-  caja.style.background = "Red";
+  caja.style.background = "Red"; // Cambia el color de fondo a rojo al quitar el mouse de la caja
 }
 function eventoDown(caja) {
-  caja.innerText = "Hola mundo";
+  caja.innerText = "Hola mundo"; // Cambia el texto de la caja al hacer click y mantenerlo presionado
 }
 function eventoUp(caja) {
-  caja.innerText = "Adios mundo";
+  caja.innerText = "Adios mundo"; // Cambia el texto de la caja al soltar el click después de haberlo mantenido presionado
 }
 function eventoInput(input) {
   console.log(input.value); // Mostar input en consola en vez de mostralo en el front o en la pagina
@@ -19,5 +19,5 @@ function eventoInput(input) {
 }
 
 function eventoDouble(caja) {
-  caja.innerText = "Haz hecho doble click, muy bien.";
+  caja.innerText = "Haz hecho doble click, muy bien."; // Cambia el texto de la caja al hacer doble click
 }
