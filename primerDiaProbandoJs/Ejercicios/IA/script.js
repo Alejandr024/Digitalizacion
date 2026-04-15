fae52d8637ef694bd840db50237c45ebf4a6c22f
Alejandro <alej3839@gmail.com>
2026-04-15 16:28:01 +0100

    Actividad IA
    Iniciamos una actividad, que combina todas las partes del frontEnd: HTML, CSS y JS (15/04/2026)

---
+++ primerDiaProbandoJs/Ejercicios/IA/script.js
@@ -0,0 +1,85 @@
+function eventoClickParaQueSirve(cajas) {
+  cajas.style.color= "black";
+  cajas.innerText = "Su uso es cotidiano en el ambito de la programación, ya sea se desarrollo web o de software.";
+  cajas.style.background= "lightBlue";
+  cajas.style.fontFamily= "Roboto";
+}
+
+function eventoOutSituaciones(cajas) {
+  cajas.style.color = "Black";
+  cajas.style.background = "lightBlue";
+  cajas.innerText= "Utilizado para automatizar tareas, mejorar la eficiencia, analizar datos, entre otras cosas.";
+  cajas.style.fontFamily= "Roboto";
+}
+
+function eventoDoubleEjemplo(cajas) {
+  cajas.style.color= "Black";
+  cajas.innerText = "Que corrija un error de bucle infinito \"while\".";
+  cajas.style.fontFamily= "Roboto";
+  cajas.style.background= "lightBlue";
+}
+
+function eventoOverFuncionConcreta(cajas) {
+  cajas.style.color= "Black";
+  cajas.style.background= "lightgreen";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText = "En todos los casos, si no le dices ninguna indicación, Claude te responde con el código corregido, sin tu decirle nada.";
+}
+
+function eventoOut(cajas) {
+  cajas.style.color = "Black";
+  cajas.style.background = "lightBlue";
+  cajas.innerText= "De web";
+}
+
+function eventoDownSinIa(cajas) {
+  cajas.style.color= "Black";
+  cajas.style.background= "lightGreen";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText = "Sin la IA, muchos problemas o aplicaciones tardarían más de ser resueltas o desarrolladas.";  
+}
+
+function eventoOutDatosIa(cajas) {
+  cajas.style.color = "Black";
+  cajas.style.background = "Orange";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "Utiliza el historial de conversación de los usuario, y los envía a los servidores de Anthropic.";
+}
+
+function eventoClickDeDondeSonLosDatos(cajas) {
+  cajas.style.color = "Black";
+  cajas.style.background = "Orange";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "Como se dijo anteriormente, los datos provienen del historial de conversación de los usuarios.";
+}
+
+function eventoDoubleDatosErroneos(cajas) {
+  cajas.style.color= "Black";
+  cajas.style.background = "Orange";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "En el caso de que se le pasen datos erróneos a Claude, este, al igual que otra IA, puede generar respuestas alucinadas, en otras palabras, respuestas, que pueden ser convincentes, pero son erroneas.";
+}
+
+function eventoOverVentajas(cajas) {
+  cajas.style.color= "Black";
+  cajas.style.background = "#DAB1DA";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "Algunas de las ventajas de la AI puede ser: automatización de tareas, con el uso del \"vibe coding\", ahorro de tiempo a la hora de escribir código, entre otras cosas.";
+}
+
+function eventoDownBeneficioIa(cajas){
+  cajas.style.color= "Black";
+  cajas.style.background = "#DAB1DA";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "El beneficio de la IA es, siempre y que nunca cambia, para las empresas, ya que con la IA, las empresas ahorran en sueldos de empleados, que pueden invertir en la IA, que les sale más barato.";
+}
+
+
+function eventoOverRiesgosIa(cajas){
+  cajas.style.color= "Black";
+  cajas.style.background = "Pink";
+  cajas.style.fontFamily= "Roboto";
+  cajas.innerText= "Los riesgos de la IA pueden ser: errores de código hecho por la IA, .";
+}
+
+//document.getElement(seleciona)ById("nombreId")(el parrafo o lo que tenga el id).textConcent(lugar donde escribe el texto)= input.value(recoge lo que escribe el usuario);
